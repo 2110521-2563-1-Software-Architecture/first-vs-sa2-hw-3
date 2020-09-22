6c6215f5753f5b43b3dfab762415ef6cd3a825e8
nictrak <narin14175@gmail.com>
2020-09-22 21:09:27 +0700

    mvvm added

--- mvvm/mvvm/view_models/main_view_model.py
+++ mvvm/mvvm/view_models/main_view_model.py
@@ -14,13 +14,22 @@ class MainViewModel:
         self.note_repository = NoteRepository()
         # Create notes field as a behavior subject with note from the business logic as an initial value
         # Your code here
+        self.observables = []
+
+    def attach(self, observable):
+        self.observables.append(observable)
+        observable.update_view(self.note_repository.notes)
 
     def add_note(self, note: str):
         # Add note and emit event with new date to the subject
         # Your code here
-        pass
+        self.note_repository.add_note(note)
+        for o in self.observables:
+            o.update_view(self.note_repository.notes)
 
     def clear_all(self):
         # Clear all note and emit event with new data to the subject
         # Your code here
-        pass
+        self.note_repository.clear_all_notes()
+        for o in self.observables:
+            o.update_view(self.note_repository.notes)
